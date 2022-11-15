d0ab602e78d0bcf7e944df27c7e952860181f424
Hannele Ruiz <justlemoncl@gmail.com>
2022-11-15 15:17:16 -0300

    Added fetching of natives

--- nativegen.py
+++ nativegen.py
@@ -1,7 +1,15 @@
+import sys
 from argparse import ArgumentParser
 from pathlib import Path
 
+import requests
+
 URL = "https://github.com/justalemon/NativeGen"
+NATIVES = {
+    "gtav": "https://raw.githubusercontent.com/alloc8or/gta5-nativedb-data/master/natives.json",
+    "rdr3": "https://raw.githubusercontent.com/alloc8or/rdr3-nativedb-data/master/natives.json",
+    "fivem": "https://runtime.fivem.net/doc/natives_cfx.json"
+}
 
 
 def parse_arguments():
@@ -12,10 +20,27 @@ def parse_arguments():
                         help="the output file")
     parser.add_argument("format", choices=["shvdn", "cfxlua"],
                         help="the format of the file")
-    parser.add_argument("--natives", choices=["gtav", "rdr2", "fivem"], nargs="+", default=["gtav"],
+    parser.add_argument("--natives", choices=list(NATIVES.keys()), nargs="+", default=["gtav"],
                         help="the different sets of natives to add")
 
     return parser.parse_args()
+
+
+def fetch_natives(natives: list[str]):
+    result = {}
+
+    for native in natives:
+        url = NATIVES[native]
+        response = requests.get(url)
+
+        if not response.ok:
+            print(f"Unable to fetch {url}", file=sys.stderr)
+            sys.exit(3)
+
+        data = response.json()
+        result[native] = data
+
+    return result
 
 
 def main():
@@ -25,6 +50,8 @@ def main():
 
     print(f"Saving Natives to {path}")
     print(f"Using format {arguments.format} and natives {arguments.natives}")
+
+    natives = fetch_natives(arguments.natives)
 
     return
 
